bad99e9aaeaf7100931e55cacb1791a84785c49b
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-09-15 02:58:22 -0700

    Add User model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,4 @@
-#!/usr/bin/python3
-"""Defines the FileStorage class for JSON serialization."""
 import json
-from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""User class module."""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User."""
+    """User class."""
 
     email = ""
     password = ""
