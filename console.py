3255df3ea5b7591c21dd8be194cfbc15ef7f03be
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-09-15 02:40:36 -0700

    Update console with CRUD commands

--- console.py
+++ console.py
@@ -2,12 +2,20 @@
 """Command interpreter for the AirBnB project."""
 
 import cmd
+import shlex
+
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for the AirBnB project."""
 
     prompt = '(hbnb) '
+
+    classes = {
+        'BaseModel': BaseModel,
+    }
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -21,6 +29,152 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when an empty line is entered."""
         pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel."""
+        args = shlex.split(arg)
+
+        if not args:
+            print('** class name missing **')
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        instance = self.classes[class_name]()
+        instance.save()
+        print(instance.id)
+
+    def do_show(self, arg):
+        """Show an instance based on class name and id."""
+        args = shlex.split(arg)
+
+        if not args:
+            print('** class name missing **')
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print('** instance id missing **')
+            return
+
+        instance_id = args[1]
+        key = '{}.{}'.format(class_name, instance_id)
+        instance = storage.all().get(key)
+
+        if instance is None:
+            print('** no instance found **')
+            return
+
+        print(instance)
+
+    def do_destroy(self, arg):
+        """Delete an instance based on class name and id."""
+        args = shlex.split(arg)
+
+        if not args:
+            print('** class name missing **')
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print('** instance id missing **')
+            return
+
+        instance_id = args[1]
+        key = '{}.{}'.format(class_name, instance_id)
+
+        if key not in storage.all():
+            print('** no instance found **')
+            return
+
+        del storage.all()[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Print all string representations of instances."""
+        args = shlex.split(arg)
+
+        if args and args[0] not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        objects = storage.all()
+
+        if args:
+            class_name = args[0]
+            result = [
+                str(obj) for key, obj in objects.items()
+                if key.startswith(class_name + '.')
+            ]
+        else:
+            result = [str(obj) for obj in objects.values()]
+
+        print(result)
+
+    def do_update(self, arg):
+        """Update an instance with an attribute and value."""
+        args = shlex.split(arg)
+
+        if not args:
+            print('** class name missing **')
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print('** instance id missing **')
+            return
+
+        instance_id = args[1]
+        key = '{}.{}'.format(class_name, instance_id)
+        instance = storage.all().get(key)
+
+        if instance is None:
+            print('** no instance found **')
+            return
+
+        if len(args) < 3:
+            print('** attribute name missing **')
+            return
+
+        attribute_name = args[2]
+
+        if len(args) < 4:
+            print('** value missing **')
+            return
+
+        value = args[3]
+
+        if hasattr(instance, attribute_name):
+            current_value = getattr(instance, attribute_name)
+
+            if isinstance(current_value, bool):
+                value = value.lower() == 'true'
+            elif isinstance(current_value, int):
+                value = int(value)
+            elif isinstance(current_value, float):
+                value = float(value)
+
+        setattr(instance, attribute_name, value)
+        instance.save()
 
 
 if __name__ == '__main__':
