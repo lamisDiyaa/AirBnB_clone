2016c8f42e84f06030643f116fb323cc208d4537
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-09-15 04:25:40 -0700

    Update console for all models

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ import shlex
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -17,6 +22,11 @@ class HBNBCommand(cmd.Cmd):
     classes = {
         'BaseModel': BaseModel,
         'User': User,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review,
     }
 
     def do_quit(self, arg):
